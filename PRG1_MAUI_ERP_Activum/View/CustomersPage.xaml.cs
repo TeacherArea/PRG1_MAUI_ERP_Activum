ae441d4d18d03abe903174515762ed0aaf001206
TeacherArea <sewilius.berg@initialize.se>
2026-04-15 09:38:05 +0200

    Change branchname and its content to better match the projects issues

--- PRG1_MAUI_ERP_Activum/View/CustomersPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/CustomersPage.xaml.cs
@@ -15,17 +15,17 @@ namespace PRG1_MAUI_ERP_Activum.View
             CustomersCollection.ItemsSource = _service.Customers;
         }
 
-        private void Customers_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var selected = e.CurrentSelection.FirstOrDefault() as Customer;
-            if (selected == null)
-            {
-                CustomerInsurances.ItemsSource = null;
-                return;
-            }
+        //private void Customers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    var selected = e.CurrentSelection.FirstOrDefault() as Customer;
+        //    if (selected == null)
+        //    {
+        //        CustomerInsurances.ItemsSource = null;
+        //        return;
+        //    }
 
-            CustomerInsurances.ItemsSource = _service.GetInsurancesForCustomer(selected).ToList();
-        }
+        //    CustomerInsurances.ItemsSource = _service.GetInsurancesForCustomer(selected).ToList();
+        //}
 
         private void AddCustomer_Clicked(object sender, EventArgs e)
         {
